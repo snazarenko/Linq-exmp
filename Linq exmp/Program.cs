7ddaa47cad3fcdaa9cd03e867e6fd007f8651601
snazarenko <serh.nazarenko@ya.ru>
2016-11-28 16:23:17 +0200

    Добавьте файлы проекта.

---
+++ Linq exmp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_exmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            
+            Array.Sort(files, new FileInfoComparer());
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
